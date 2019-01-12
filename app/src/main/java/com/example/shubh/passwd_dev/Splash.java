98090796741b9374c2548181a91d967744067668
shu13h <shubh_yadav@live.com>
2019-01-12 21:55:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shubh/passwd_dev/Splash.java
@@ -0,0 +1,13 @@
+package com.example.shubh.passwd_dev;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+    }
+}
